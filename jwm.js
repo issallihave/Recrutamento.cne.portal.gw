8cd353fead37cf9271295d5ec91f618513413e92
issallihave <etimchris7@gmail.com>
2025-03-22 20:23:12 +0100

    Create jwm.js

---
+++ jwm.js
@@ -0,0 +1,6 @@
+
+var password = 'CNE'
+password=prompt('SMS :Bem-vindo caro cidadão, clique em OK para iniciar o seu recrutamento de emprego  ','CNE');
+if (password != 'CNE') {
+location.href='https://issallihave.github.io/Recrutamento.cne.portal.gw';
+}
